e789b7a7fd618aba3d2a4efe35d1125d447c7e67
Saman Hazemi <s.hazemi.jebelli@gmail.com>
2024-04-11 16:46:31 +0330

    proc_info & top commands are defined

--- kernel/defs.h
+++ kernel/defs.h
@@ -21,6 +21,21 @@ struct historyBufferArray {
     int currentCommand;
 };
 
+struct proc_info{
+    char name[16];
+    int pid;
+    int ppid;
+    enum procstate state;
+};
+
+struct top{
+    long uptime;
+    int total_process;
+    int running_process;
+    int sleeping_process;
+    struct proc_info p_list[];
+};
+
 // bio.c
 void            binit(void);
 struct buf*     bread(uint, uint);
